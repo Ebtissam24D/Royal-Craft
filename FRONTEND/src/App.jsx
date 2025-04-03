97c2ad4356ea1934fff6bbdb42a8dfe9fcfabd12
Ibtissam <ibassma039@gmail.com>
2025-04-03 17:33:32 +0000

    commit BOUADAME

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,15 @@
+import Layout from "./components/Layout"
+import "./index.css"
+import HomePage from "./pages/Homepage"
+
+function App() {
+  return (
+    <Layout>
+        <HomePage />
+        {/* Ajoutez d'autres pages ici si nécessaire */}
+    </Layout>
+  )
+}
+
+export default App
+
---
+++ FRONTEND/src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+
+
+function Layout({ children }) {
+  return (
+    <div className="layout">
+      <Header />
+      <main className="main-content">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
+
