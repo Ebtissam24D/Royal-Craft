d21f40dcdbc3aceb2baad517509fa1f7183905b2
Ibtissam <ibassma039@gmail.com>
2025-04-10 01:14:49 +0100

    commit Bouadame

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,34 +1,12 @@
-import Layout from "./components/Layout"
 import "./index.css"
-// import HomePage from "./pages/Homepage"
-// import Products from "./pages/ProduitPage/Products.jsx"
-// import Recommended from "./pages/ProduitPage/Recommended.jsx"
-// import Category from "./pages/ProduitPage/Category.jsx"
-// import Colors from "./pages/ProduitPage/Colors.jsx"
-// import Price from "./pages/ProduitPage/Price.jsx"
-// // import Sidebar from "./pages/ProduitPage/Sidebar.jsx"
-// import "./app.css"; // Ajoute ce fichier pour le layout
-// import Catalogue from "./pages/ProduitPage/Catalogue.jsx"
-import PlusdedetailleP from "./pages/PlusDedetailleProduit/Plusdedetaille"
+import HomePage from "./pages/Homepage"
+
 
 
 
 function App() {
   return (
-    <Layout>
-      {/* // <LayoutArtisan> */}
-      {/* <HomePage /> */}
-
-      <PlusdedetailleP />
-      {/* <Catalogue />
-
-    // <Recommended />
-    // <Category />
-    // <Colors />
-    // <Price /> */}
-
-      {/* </LayoutArtisan> */}
-    </Layout>
+      <HomePage />
   )
 }
 
